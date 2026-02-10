f2797f040b510f520c5798835ca672e5bafefb26
Edward <snegovikzimniy@gmail.com>
2026-02-10 16:46:16 +0300

    javascript interview prep: function declaration, function expression, arrow function. difference

---
+++ Interview Preparing/JavaScript/Темы/4. Отличия объявления функций/script.js
@@ -0,0 +1,23 @@
+
+// Function Declaration
+function function_declaration() {
+
+}
+
+
+
+// Function Expression
+function_expression() // Нельзя вызвать до инициализации
+
+const function_expression = function() {
+    console.log('Здарова красава')
+}
+
+
+
+// Arrow Function
+arrow_function() // Нельзя вызвать до иницализации
+
+const arrow_function = () => {
+
+}
